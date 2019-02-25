c6b796e8acdd9e64796e58c17a1875ef628bb5bb
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-02-25 18:52:28 -0500

    created comments page with new comment page

---
+++ DartmouthNaps/app/src/main/java/edu/dartmouth/cs65/dartmouthnaps/LocationActivity.java
@@ -0,0 +1,71 @@
+package edu.dartmouth.cs65.dartmouthnaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class LocationActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        imageView = findViewById(R.id.location_image);
+        listView = findViewById(R.id.comment_list_view);
+
+        // Customizing Image View
+        imageView.getLayoutParams().height = 620;
+
+        // Populating List of Comments
+        ArrayList<Comment> comments = getComments();
+        ListView listView = this.findViewById(R.id.comment_list_view);
+        listView.setAdapter(new CommentAdapter(this, comments));
+
+    }
+
+    public void onAddComment(View view) {
+        Intent intent = new Intent(this, NewCommentActivity.class);
+        startActivity(intent);
+    }
+
+    private ArrayList<Comment> getComments() {
+        ArrayList<Comment> comments = new ArrayList<>();
+        Comment comment = new Comment();
+
+        comment.setAuthor("XD");
+        comment.setDateTime(new Date());
+        comment.setLocation("First Floor 007");
+        comment.setBody("this is a terrible location to sleep during 12s");
+        comments.add(comment);
+
+        comment.setAuthor("XD");
+        comment.setDateTime(new Date());
+        comment.setLocation("First Floor 007");
+        comment.setBody("this is a terrible location to sleep during 12s");
+        comments.add(comment);
+
+        comment.setAuthor("XD");
+        comment.setDateTime(new Date());
+        comment.setLocation("First Floor 007");
+        comment.setBody("this is a terrible location to sleep during 12s");
+        comments.add(comment);
+
+        comment.setAuthor("XD");
+        comment.setDateTime(new Date());
+        comment.setLocation("First Floor 007");
+        comment.setBody("this is a terrible location to sleep during 12s");
+        comments.add(comment);
+
+        return comments;
+    }
+}
