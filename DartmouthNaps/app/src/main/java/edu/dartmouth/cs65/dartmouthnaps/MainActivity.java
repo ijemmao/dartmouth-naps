392270ce0c1afc4f418cb70d330dc8aa0ed79e6a
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-03-01 19:29:10 -0500

    created fragment that looks like designs

--- DartmouthNaps/app/src/main/java/edu/dartmouth/cs65/dartmouthnaps/MainActivity.java
+++ DartmouthNaps/app/src/main/java/edu/dartmouth/cs65/dartmouthnaps/MainActivity.java
@@ -1,8 +1,6 @@
 package edu.dartmouth.cs65.dartmouthnaps;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,13 +9,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import android.widget.LinearLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import edu.dartmouth.cs65.dartmouthnaps.models.Place;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         user = auth.getCurrentUser();
         dbReference = FirebaseDatabase.getInstance().getReference();
 
-        if(user == null) { //if noone is logged in, go to login activity
+        if(user == null) { //if none is logged in, go to login activity
             Intent intent = new Intent(this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
@@ -43,19 +34,6 @@ public class MainActivity extends AppCompatActivity {
             uID = user.getUid();
             TextView welcomeUser = (TextView) findViewById(R.id.text_welcome_user);
             welcomeUser.setText("Welcome User " + uID);
-        }
-
-        LinearLayout linearLayout = findViewById(R.id.color_parent);
-        List<TextView> textViews = new ArrayList<>();
-        List<Place> Place = new ArrayList<>();
-        for (int i = 0; i < 21; i++) {
-            textViews.add((TextView)linearLayout.getChildAt(i));
-            Place.add(new Place());
-            Map<String, Integer> ratings = Place.get(i).getRatings();
-            for (int j = 0; j < 21; j++) {
-                ratings.put("" + j, j <= i ? 5 : 0);
-            }
-            textViews.get(i).setBackgroundColor(Place.get(i).ratingColor());
         }
     }
 }
