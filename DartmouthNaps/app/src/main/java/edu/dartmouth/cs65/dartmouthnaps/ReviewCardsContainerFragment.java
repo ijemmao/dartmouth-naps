781769d2f8fab723c2a03b2aee0d316f321144dd
ijemmao <ijemmaonwuzulike10@gmail.com>
2019-03-01 19:46:00 -0500

    able to swipe between different cards

---
+++ DartmouthNaps/app/src/main/java/edu/dartmouth/cs65/dartmouthnaps/ReviewCardsContainerFragment.java
@@ -0,0 +1,71 @@
+package edu.dartmouth.cs65.dartmouthnaps;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ReviewCardsContainerFragment extends Fragment {
+
+    private static final int NUM_PAGES = 5;
+
+    private ViewPager mPager;
+    private PagerAdapter pagerAdapter;
+
+    public ReviewCardsContainerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_review_cards_container, container, false);
+
+        mPager = (ViewPager) view.findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+
+        return view;
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        if (mPager.getCurrentItem() == 0) {
+//            // If the user is currently looking at the first step, allow the system to handle the
+//            // Back button. This calls finish() on this activity and pops the back stack.
+//            super.onBackPressed();
+//        } else {
+//            // Otherwise, select the previous step.
+//            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+//        }
+//    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ReviewCardFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
+}
